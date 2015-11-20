0b90c84f68dab6f4415b07952a54468539bbd097
Glen <glencbz@gmail.com>
2015-11-21 00:33:54 +0800

    added highlight and ignore options

--- edimensionReverser.user.js
+++ edimensionReverser.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         eDimension Reverser
 // @namespace    https://github.com/glencbz/edimensionReverser
-// @version      0.1
+// @version      0.6
 // @description  Reverses the course list on eDimension
 // @author       Glen Choo
 // @match        http://edimension.sutd.edu.sg/
@@ -12,6 +12,38 @@
 /* jshint -W097 */
 'use strict';
  
+var highlightCourses = [
+	"optimization",
+	"machine",
+	"database",
+	"the mind",
+];
+
+var ignore =[
+	"survey",
+	"capstone"
+];
+
+highlightCourses = highlightCourses.map(function(obj){
+	return obj.toLowerCase();
+});
 var $ = window.jQuery;
+var styles = "<style>.highlighted{background-color: #E8F6FF;}</style>";
+$("body").append(styles);
+
 var courseList = $(".unlist");
-courseList.children().each(function(i, li){courseList.prepend(li)});
+courseList.children().each(function(i, li){
+	var $li = $(li);
+	var titleLower = $li.find(".name>a").text().toLowerCase();
+	for (var i = 0; i < highlightCourses.length; i++){
+		if (titleLower.indexOf(highlightCourses[i]) > -1){
+			$li.children(".coursebox").addClass("highlighted");
+			break;
+		}
+	}
+	for (var i = 0; i < ignore.length; i++){
+		if (titleLower.indexOf(ignore[i]) > -1)
+			return;
+	}
+	courseList.prepend(li);
+});
